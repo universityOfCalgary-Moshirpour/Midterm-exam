5a2606c59075bd0be66cdd07cf07903f981bbf2f
ronakBarati <37188279+ronakBarati@users.noreply.github.com>
2022-11-04 08:47:34 -0600

    Initial commit

---
+++ src/exercise1and2/Colour.java
@@ -0,0 +1,21 @@
+package exercise1and2;
+
+class Colour
+{
+    private String colour;
+    
+	public Colour(String s) {
+		colour = new String(s);
+	}
+
+
+    public void setColour(String newColour){
+    	colour = newColour;
+    }
+    
+	@Override
+	public String toString(){
+		return colour;
+	}
+
+}
